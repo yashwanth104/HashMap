0ff45828f864eedad61d7e00c4f1a589e40fdcec
DESKTOP-RN2K5O7\Yashwanth <gupta.yashwanth@gmail.com>
2018-12-02 23:48:35 +0100

    To test API after developing jar and adding to dependencies in test

---
+++ testingAPI/src/testingAPI/jdhzd.java
@@ -0,0 +1,12 @@
+package testingAPI;
+//
+import hashmPackage.HashAPI;
+
+public class jdhzd {
+	public static void main(String[] args) {
+  		HashAPI<String, Double>map = new HashAPI<String, Double>(); 
+  		map.put("yash",2.0 ); 
+  		System.out.println(map.get("yash"));
+
+  }
+}
